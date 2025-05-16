51bb35e5d6aa375947b280a7211bd02d8da234d5
zahwaAfifah <zahwa.afifah@student.alhazen.academy>
2025-05-16 08:59:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let angka1 = prompt("masukkan angkapertama:")
+let angka2 = prompt("masukkan angka kedua")
+
+angka1 = parseFloat(angka1)
+angka2 = parseFloat(angka2)
+
+let hasil = angka1 + angka2
+alert("hasil penjumlahan:" + hasil)
